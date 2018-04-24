e80cf6f88b685f7a3db20cc499d4cc555f58f5e5
nandini10 <nandini2@illinois.edu>
2018-04-24 14:43:58 -0500

    folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Sidebar from 'react-sidebar';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <p className="App-intro"></p>
+    );
+  }
+};
+
+export default App;
